e3c68b48fa1c153acb2c82b5f50cfa407c5d4a6f
lj <1007515184@qq.com>
2024-05-25 16:34:44 +0800

    update arm template

---
+++ asm/arm/header.h
@@ -0,0 +1,14 @@
+#ifndef __HEADER_H__
+#define __HEADER_H__
+#define UART_BASE 0x13000000
+
+#define EL0_T 0x0
+#define EL1_T 0x4
+#define EL1_H 0x5
+#define EL2_T 0x8
+#define EL2_H 0x9
+#define EL3_T 0xc
+#define EL3_H 0xd
+
+
+#endif
--- asm/arm/test.c
+++ asm/arm/test.c
@@ -1,10 +1,10 @@
-extern unsigned long int* UART_BASE_ADDR;
+#include "header.h"
 
 
 
 int main()
 {
-	unsigned long int temp = *(unsigned long int*)UART_BASE_ADDR;
+	unsigned long int temp = *(unsigned long int*)UART_BASE;
 	unsigned long int temp2 = temp + temp;
 	return (int)temp2;
 }
